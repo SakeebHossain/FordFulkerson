263834e1853f4f575268be26e58c0db4ab4e3776
SakeebHossain <sakeeb.hossain1@gmail.com>
2017-01-04 10:36:10 -0500

    few methods left, looking good.

--- FordFulkerson.js
+++ FordFulkerson.js
@@ -6,34 +6,64 @@ function Graph() {
 
     this.addNode = function(id) {
         //make sure id is unique
-        for (i = 0; i < idList.length; i++) {
-            if (idList[i] == id) {
+        for (i = 0; i < this.idList.length; i++) {
+            if (this.idList[i] == id) {
                 console.log("A node with id", id, "already exists!");
                 return;
             };
         };
         //initialize new row with -inf's based on len of idlist
         var newRow = [];
-        for (i = 0; i < idList.length; i++) {
-            newRow[i].append(-Infinity);
+        for (i = 0; i < this.idList.length; i++) {
+            newRow[i] = -Infinity;
         };
         //add new row (append new list)
         this.adjMatrix.push(newRow);
         //add new id to idList
         this.idList.push(id);
         //add new columns in each row (go through each row in matrix, append -inf) to matrix
-        for (i = 0; i < idList.length; i++) {
+        for (i = 0; i < this.idList.length; i++) {
             this.adjMatrix[i].push(-Infinity);
         };
     };
 
-    //deleteNode(id)
-    /*
-    -confirm node exists
-    -find its index in the idList, i
-    -remove ith row in adjMatrix
-    -splice out ith element from all other arrays
-    */
+    this.deleteNode = function(id) {
+        var found = false;
+        var index;
+        var k;
+        
+        //confirm node exists
+        for (i = 0; i < this.idList.length; i++) {
+            if (this.idList[i] == id) {
+                    found = true;
+                    break;
+                };
+        };
+
+        if (found == false) {
+            console.log("The node with id", id, "wasn't found.");
+            return;
+        };
+
+        //if found, find its index in the idList, k
+        for (i = 0; i < this.adjMatrix.length; i++) {
+            if (this.idList[i] == id) {
+                index = i;
+                break;
+            }
+        };
+
+        //splice out the kth element in idList
+        this.idList.splice(index, 1);
+
+        //splice out the kth row in adjMatrix
+        this.adjMatrix.splice(index, 1);
+
+        //splice out kth element from all other arrays
+        for (i = 0; i < this.adjMatrix.length; i++) {
+            this.adjMatrix[i].splice(k, 1);
+        };
+    };
 
     //addEdge(id1, id2)
     /*
@@ -50,10 +80,26 @@ function Graph() {
     */
 
     //print
-    /*
-    -print very row of adjMatrix
-    */
+    this.getDetails = function() {
+        console.log(this.idList);
+        for (i = 0; i < this.adjMatrix.length; i++) {
+            console.log(this.idList[i], this.adjMatrix[i], "\n");
+        };
+        console.log("\n");
+    };
+
+    //getedges() : returns a list of edges in the format (from, to)
 };
 
 //-------------------------------------------------------------------------------------------------
 //TESTS--------------------------------------------------------------------------------------------
+var G = new Graph();
+//Testing Graph.addNode
+G.addNode("node1");
+G.addNode("node2");
+G.addNode("node3");
+G.addNode("node2"); //should fail
+G.getDetails();
+//Testing Graph.deleteNode
+G.deleteNode("node2");
+G.getDetails();
